e55cd3276ea460865413c80ecc427f68dbd6d101
Yulia Tsareva <tsarovayulia@gmail.com>
2024-05-24 14:34:56 +0000

    cleanup

--- yt/yt/server/all/fuzztests/distributed/fuzz-distributed.cpp
+++ yt/yt/server/all/fuzztests/distributed/fuzz-distributed.cpp
@@ -94,26 +94,21 @@ bool IsValidChunkType(const NYT::NChunkClient::NProto::TSessionId& protoSessionI
     }
 }
 
+
 static protobuf_mutator::libfuzzer::PostProcessorRegistration<NYT::NChunkClient::NProto::TSessionId> NonNullSessionId = {
     [](NYT::NChunkClient::NProto::TSessionId* message, unsigned int seed) {
-        fuzzing::Timer t;
         // Fixes 'No write location is available'
         message->set_medium_index(0);
 
         // Fixes 'Invalid session chunk type'
-        std::mt19937_64 rng(seed);
-        std::uniform_int_distribution<uint64_t> dist64(1, UINT64_MAX);
-
-        bool isValidChunkType = IsValidChunkType(*message);
-        while (!isValidChunkType) {
-            NYT::NProto::TGuid randomChunkId;
-            randomChunkId.set_first(dist64(rng));
-            randomChunkId.set_second(dist64(rng));
-            message->mutable_chunk_id()->CopyFrom(randomChunkId);
-
-            isValidChunkType = IsValidChunkType(*message);
-        };
-        YT_LOG_INFO("PostProcessor TSessionId took %v ms", t.Reset());
+        if (!IsValidChunkType(*message)) {
+            auto first = message->mutable_chunk_id()->first();
+            uint64_t h = 100;
+            h <<= 32;
+            first = (first & 0xFFFFFFFF) | h;
+            message->mutable_chunk_id()->set_first(first);
+        }
+        YT_VERIFY(IsValidChunkType(*message));
     }};
 
 
@@ -150,6 +145,18 @@ void SendRequest(const std::string& methodName, const TRequest& request, TProxyM
     // if (methodName != "GetChunkFragmentSet") return;
     YT_LOG_INFO("LOOOG Sending %v, attachments size: %v", methodName, attachments.size());
     std::cerr << "LOOOG Sending " << methodName << ", attachments size: " << attachments.size() << std::endl;
+    {
+        google::protobuf::TextFormat::Printer printer;
+        printer.SetUseUtf8StringEscaping(true);
+
+        TProtoStringType text_message;
+        std::cout << "Fuzzer input:\n";
+        if (!printer.PrintToString(request, &text_message)) {
+            std::cerr << "Failed to convert protobuf message to text." << std::endl;
+            return;
+        }
+        std::cerr << methodName << ":\n" << text_message << std::endl;
+    }
     std::string rspMsg = "";
     // size_t retry = 0;
     // // do {
